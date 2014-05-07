95990aa723fe99906a6d78726d163adcc5ebf512
Noureddin Sadawi <n.sadawi@gmail.com>
2014-05-07 11:35:25 +0100

    renamed query.html to index.php and added endpoint.txt

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-I will update this regularly
-Noureddin
+- this php sparql client uses the "Simple library to query SPARQL from PHP sparqllib.php"
+- more info here: http://graphite.ecs.soton.ac.uk/sparqllib/
+
+- index.php has the form where we can specify a SPARQL endpoint and a query (it reads endpoint name from 1st line of endpoint.txt)
+- p.php processes the the request and displays the results
+- endpoint.txt has a preset endpoint name (only 1st line will be read)
--- index.php
+++ index.php
@@ -2,34 +2,36 @@
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="stylesheet" type="text/css" media="screen" href="istyle.css"/>
+<link rel="stylesheet" type="text/css" media="screen" href="style.css"/>
 <title>SPARQL Query Form</title>
 
 </head>
-         <body> 
-           <form action="q.php" method="post"> 
+   <body> 
+   <?php 
+     //PHP code to read endpoint address from textfile
+     //endpoint address must be the first line (code only reads the first line)
+     $file="endpoint.txt"; $var=fopen($file,"r"); $endpoint=fgets($var); fclose($var); 
+   ?> 
+      <form action="q.php" method="post"> 
 
-		<div>
-		<h1>SPARQL Query Form :</h1>
-		<label>
-		<span>SPARQL Endpoint</span><input id="name" type="text" name="endpoint"/>
-		</label>
-					
-		<label>
-		<span>SPARQL Query</span>
-		  <textarea id="feedback" name="sparql">  </textarea>
-		</label>
+	<div>
+ 	  <h1>SPARQL Query Form :</h1>
+	   <label>
+	    <span>SPARQL Endpoint</span><input id="name" type="text" value="<?php  echo $endpoint; ?>" name="endpoint"/>
+	   </label>
+				
+	   <label>
+	    <span>SPARQL Query</span>
+	    <textarea id="feedback" name="sparql">  </textarea>
+	   </label>
 		
-		<input type="submit" value="Execute!" />
+	   <input type="submit" value="Execute!" />
 		
-		
+	</div>
 
-
-		</div>
-
-           </form> 
-         </body> 
-       </html> 
+      </form> 
+   </body> 
+</html> 
        
  
  
