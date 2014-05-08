6bc1d306abdee1d5bae9ef19e349f725988f3210
Noureddin Sadawi <n.sadawi@gmail.com>
2014-05-08 11:43:33 +0100

    updated index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 				
 	   <label>
 	    <span>SPARQL Query</span>
-	    <textarea id="sparql" name="sparql_area" text="hala">  </textarea>
+	    <textarea id="sparql" name="sparql">  </textarea>
 	   </label>
 		
 	   <input type="submit" value="Execute!" />		
@@ -34,7 +34,13 @@
 	 </div>
 	 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="fill_sparql.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+	  $("h1").click(function() {
+	    $('#sparql').html("prefix owl: <http://www.w3.org/2002/07/owl#> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> prefix pho: <http://rdf.phibase.org/ontology/phibase-rdf-ontology.owl#> prefix pcore: <http://purl.uniprot.org/core/> prefix xsd: <http://www.w3.org/2001/XMLSchema#> select ?interaction ?org ?roleType ?roleLabel where { ?interaction a pho:interaction . ?interaction pho:has_participant ?org . ?org a pho:organism . ?org pho:has_role [ a ?roleType ;   pho:pho:participant_of ?interaction ;   rdfs:label ?roleLabel ] } ");   }); });
+	</script>
+	
+	
 
       </form> 
    </body> 
